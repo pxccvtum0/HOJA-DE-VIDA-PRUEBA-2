33e2fefbc6cbd714a5317c5ef82279906531fd50
pxccvtum0 <pxccvtum0@gmail.com>
2026-01-29 20:04:44 -0500

    vxirsjjkn2

--- ProyectoHojaDeVida/manage.py
+++ ProyectoHojaDeVida/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoja_de_vida2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProyectoHojaDeVida.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

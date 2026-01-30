e968dbaadd7d25d2975b8a2011e0faf64716db35
pxccvtum0 <pxccvtum0@gmail.com>
2026-01-29 19:40:39 -0500

    zen2

--- build.sh
+++ build.sh
@@ -5,10 +5,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# --- LIMPIEZA DE LOGS ACTIVADA ---
+#echo "Limpiando logs de administración..."
+#echo "from django.contrib.admin.models import LogEntry; \
+#LogEntry.objects.all().delete();" \
+#| python manage.py shell
+
 # Crear/Asegurar superusuario
 echo "Asegurando superusuario..."
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
-User.objects.filter(username='Zeta').exists() or \
-User.objects.create_superuser('Zeta', 'Zeta@gmail.com', 'Zen1999')" \
+User.objects.filter(username='Milenka').exists() or \
+User.objects.create_superuser('Milenka', 'Milenka@gmail.com', 'Milenka123')" \
 | python manage.py shell
